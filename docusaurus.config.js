2e6bf52a096006ddeaa89d421c439657b6e50a3a
nold <nold@gnu.one>
2022-10-15 17:30:49 +0200

    Disabled: local search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,7 @@ module.exports = {
     },
   },
   plugins: [
-     [require.resolve('@cmfcmf/docusaurus-search-local'), {
-     }]
+     //[require.resolve('@cmfcmf/docusaurus-search-local')]
   ],
   presets: [
     [
